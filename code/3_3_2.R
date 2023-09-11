8f2249b65c4b8e19184628051ab7b826f49ee987
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2023-09-11 16:24:59 +0900

    「標準正規分布とカイ二乗分布の関係」を追加

---
+++ code/3_3_2.R
@@ -0,0 +1,209 @@
+
+# カイ二乗分布 ------------------------------------------------------------------
+
+# 利用パッケージ
+library(tidyverse)
+library(gganimate)
+
+# チェック用
+library(ggplot2)
+
+
+# 標準正規分布とカイ二乗分布の関係 ----------------------------------------------------------
+
+# 試行回数を指定
+max_iter <- 10000
+
+# サンプルサイズ(自由度)を指定
+N <- 10
+
+# (標準)正規分布のパラメータを指定:(固定)
+mu_x    <- 0
+sigma_x <- 1
+
+
+# サンプリング
+sample_x_df <- tidyr::expand_grid(
+  iter = 1:max_iter, # 試行番号
+  i    = 1:N         # データ番号:(サンプル)
+) |> 
+  dplyr::group_by(iter) |> # 乱数生成用
+  dplyr::mutate(
+    x = rnorm(n = N, mean = mu_x, sd = sigma_x)
+  ) |> 
+  dplyr::ungroup()
+
+# サンプル2乗和を計算
+sample_z_df <- sample_x_df |> 
+  # dplyr::mutate(
+  #   x = (x - mu_x) / sigma_x # 標準化
+  # ) |> 
+  dplyr::group_by(iter) |> # 平均の計算用
+  dplyr::summarise(
+    z = sum(x^2), .groups = "drop"
+  )
+
+# 確率変数の範囲を設定
+x_min <- min(sample_x_df[["x"]], sample_z_df[["z"]])
+x_max <- max(sample_x_df[["x"]], sample_z_df[["z"]])
+
+# 確率密度を計算
+dens_df <- tibble::tibble(
+  x      = seq(from = x_min, to = x_max, length.out = 1000), 
+  dens_x = dnorm(x = x, mean = mu_x, sd = sigma_x), # 正規分布
+  dens_z = dchisq(x = x, df = N) # カイ二乗分布
+) |> 
+  tidyr::pivot_longer(
+    cols = !x, 
+    names_prefix = "dens_", 
+    names_to  = "dist", 
+    values_to = "dens"
+  ) # 確率密度列をまとめる
+
+
+### ・結果の作図 -----
+
+# サンプル2乗和を抽出
+sample_z_vec <- sample_z_df[["z"]]
+
+# サンプル2乗和の標本統計量を計算:(ラベル用)
+mean_z <- sum(sample_z_vec) / max_iter
+var_z  <- sum((sample_z_vec - mean_z)^2) / max_iter
+sd_z   <- sqrt(var_z)
+
+# ラベル用の文字列を作成
+variable_label <- paste0(
+  "list(", 
+  "z[j] == sum(x[ji]^2, i==1, n)", 
+  ", nu == ", "n", 
+  ")"
+)
+stats_label <- paste0(
+  "list(", 
+  "n == ", N, 
+  ", m == ", max_iter, 
+  ", E*'['*z*']' == ", round(mean_z, digits = 3), 
+  ", V*'['*z*']' == ", round(var_z, digits = 3), 
+  ", s*'['*z*']' == ", round(sd_z, digits = 3), 
+  ")"
+)
+param_label_vec <- c(
+  parse(text = paste0("x %~% N(", mu_x, ", ", sigma_x, ")")), 
+  parse(text = paste0("z %~% chi^2*(", N, ")"))
+)
+
+# サンプルを作図
+ggplot() + 
+  geom_line(data = dens_df, 
+            mapping = aes(x = x, y = dens, color = dist), 
+            linewidth = 1) + # 生成分布
+  geom_histogram(data = sample_z_df, 
+                 mapping = aes(x = z, y = ..density..), 
+                 bins = 50, fill = scales::hue_pal()(n = 2)[2], 
+                 alpha = 0.5) + # サンプル2乗和
+  scale_color_hue(labels = param_label_vec) + # (凡例ラベル用)
+  theme(legend.text.align = 0, 
+        legend.position = "right") + 
+  labs(title = parse(text = variable_label), 
+       subtitle = parse(text = stats_label), 
+       color = "distribution", 
+       x = "value", y = "density")
+
+
+### ・推移の作図 -----
+
+# 1フレーム当たりのデータ数を指定
+n_per_frame <- 100
+
+# フレーム数を設定
+frame_num <- max_iter / n_per_frame
+frame_num
+
+
+# サンプル2乗和の標本統計量を計算:(ラベル用)
+mean_z_vec <- cumsum(sample_z_vec) / 1:max_iter
+var_z_vec  <- cumsum((sample_z_vec - mean_z_vec)^2) / 1:max_iter
+sd_z_vec   <- sqrt(var_z_vec)
+
+# サンプル2乗和を複製
+trace_sample_z_df <- sample_z_df |> 
+  tidyr::uncount(weights = (max_iter-iter)%/%n_per_frame+1, .id = "m") |> # 累積ミニバッチに複製
+  dplyr::mutate(
+    j = iter, # データ番号:(サンプル番号)
+    m = max_iter - (m-1)*n_per_frame, # 行番号をデータ数に変換
+    label = paste0(
+      "n = ", N, 
+      ", m = ", m, 
+      ", E[z] = ", round(mean_z_vec[m], digits = 3), 
+      ", V[z] = ", round(var_z_vec[m], digits = 3), 
+      ", s[z] = ", round(sd_z_vec[m], digits = 3)
+    )
+  ) |> 
+  tibble::add_row(
+    j = 0, m = 0, 
+    label = paste0("n = ", N, ", m = 0, E[z] = NA, V[z] = NA, s[z] = NA") # フレーム用ラベル
+  ) |> # 初回フレーム用にダミーを追加
+  dplyr::arrange(m, j) |> # 因子レベルの設定用
+  dplyr::mutate(
+    label = factor(label, levels = unique(label)) # フレーム番号に応じてレベル付け
+  )
+
+# サンプルを複製
+trace_sample_x_df <- sample_x_df |> 
+  dplyr::mutate(
+    frame = (iter-1) %/% n_per_frame + 1, # フレーム番号
+    m     = frame * n_per_frame, # データ数:(サンプル)
+    j     = iter, # データ番号:(サンプル2乗和)
+    color_id = (j-1) %% n_per_frame, # ミニバッチ内のデータ番号
+    label = unique(trace_sample_z_df[["label"]])[frame+1] # フレーム用ラベル
+  ) |> 
+  tibble::add_row(
+    j = 0, m = 0, 
+    label = paste0("n = ", N, ", m = 0, E[z] = NA, V[z] = NA, s[z] = NA")
+  ) |> 
+  dplyr::arrange(m, j)
+
+
+# 確率密度の最大値を設定
+y_max <- max(dens_df[["dens"]])
+
+# サンプル点のプロット位置の調整値を指定
+y_min <- -0.01
+
+# サンプルを作図
+anim <- ggplot() + 
+  geom_line(data = dens_df, 
+            mapping = aes(x = x, y = dens, color = dist), 
+            linewidth = 1) + # 生成分布
+  geom_histogram(data = trace_sample_z_df, 
+                 mapping = aes(x = z, y = ..density..), 
+                 bins = 50, fill = scales::hue_pal()(n = 2)[2], 
+                 alpha = 0.5) + # サンプル2乗和
+  geom_point(data = trace_sample_x_df, 
+             mapping = aes(x = x, y = y_min*0.5, fill = factor(color_id)), 
+             size = 2, alpha = 0.1, shape = "circle filled", stroke = 0, show.legend = FALSE) + # サンプル
+  geom_point(data = trace_sample_z_df, 
+             mapping = aes(x = z, y = y_min), 
+             size = 2, color = scales::hue_pal()(n = 2)[2], alpha = 0.01) + # サンプル2乗和
+  gganimate::transition_manual(frames = label) + # フレーム
+  scale_color_hue(labels = param_label_vec) + # (凡例ラベル用)
+  coord_cartesian(ylim = c(y_min, y_max)) + 
+  theme(legend.text.align = 0, 
+        legend.position = "right") + 
+  labs(title = parse(text = variable_label), 
+       subtitle = "{current_frame}", 
+       color = "distribution", 
+       x = "value", y = "density")
+
+# 停止フレーム数を指定
+pause_frame <- 10
+
+# gif画像を作成
+gganimate::animate(
+  plot = anim, 
+  nframes = frame_num+pause_frame, end_pause = pause_frame, fps = 10, 
+  width = 800, height = 600, 
+  renderer = gganimate::gifski_renderer()
+)
+
+
